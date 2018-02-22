19ab5397337a773d79fe4e3e96076255c27480c6
zbu <zbu@users.noreply.github.com>
2018-02-22 00:07:43 -0500

    Add files via upload

--- nsfr/nsfr.py
+++ nsfr/nsfr.py
@@ -8,19 +8,18 @@ class nsextras:
         self.bot = bot
 
     @commands.command()
-async def nsfr(self):
-    """Today's featured region"""
+    async def nsfr(self):
+        """Today's featured region"""
 
-    #Your code will go here
-    url = "https://www.nationstates.net/page=world" #build the web adress
-    async with aiohttp.get(url) as response:
-        soupObject = BeautifulSoup(await response.text(), "html.parser")
-    try:
-        online = soupObject.find(class_='featuredregion').find('a').get_text()
-        await self.bot.say(online + ' is today's featured region')
-    except:
-        await self.bot.say("ERROR.")
+        #Your code will go here
+        url = "https://www.nationstates.net/page=world" #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            online = soupObject.find(class_='featuredregion').find('a').get_text()
+            await self.bot.say(online + " is today's featured region")
+        except:
+            await self.bot.say("ERROR.")
 
 def setup(bot):
-    bot.add_cog(Mycog(bot))
-
+    bot.add_cog(nsextras(bot))
