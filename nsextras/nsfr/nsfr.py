9c5ea25d9954572c156ffc4d2b8e40b15ceaf846
zbu <zbu@users.noreply.github.com>
2018-02-21 23:41:50 -0500

    Add files via upload

---
+++ nsextras/nsfr/nsfr.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+class nsextras:
+    """NS Extras"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+async def nsfr(self):
+    """Today's featured region"""
+
+#Your code will go here
+url = "https://www.nationstates.net/page=world" #build the web adress
+async with aiohttp.get(url) as response:
+    soupObject = BeautifulSoup(await response.text(), "html.parser")
+try:
+    online = soupObject.find(class_='featuredregion').find('a').get_text()
+    await self.bot.say(online + ' is today's featured region')
+except:
+    await self.bot.say("ERROR.")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
+
