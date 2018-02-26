48bd28398b2e1fe2915945f50568f95d03fa272f
zbu <zbu@users.noreply.github.com>
2018-02-26 00:57:25 -0500

    Add files via upload

--- say/say.py
+++ say/say.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from .utils import checks
 
 class nsextras:
     """NS Extras"""
@@ -8,6 +9,7 @@ class nsextras:
         self.bot = bot
 
     @commands.command()
+    @checks.admin_or_permissions(manage_server=True)
     async def say(self, *, text):
         """Displays what is said after say"""
 
