0204cacd6bea57e5170e606c8d83f4aa4b0f124f
zbu <zbu@users.noreply.github.com>
2018-02-26 01:40:49 -0500

    Add files via upload

--- say/say.py
+++ say/say.py
@@ -10,11 +10,12 @@ class nsextras:
 
     @commands.command()
     @checks.admin_or_permissions(manage_server=True)
-    async def say(self, *, text):
+    async def say(self, channel, *, text):
         """Displays what is said after say"""
 
         #Your code will go here
-        await self.bot.say(text)
+		c = discord.utils.get(server.channels, name=channel)
+		await self.bot.send_message(c, text)
 
 def setup(bot):
     bot.add_cog(nsextras(bot))
