1a0888994c0bea54bbd6405f01b84c8ff95fd4ad
zbu <zbu@users.noreply.github.com>
2018-02-26 00:39:42 -0500

    Add files via upload

--- say/say.py
+++ say/say.py
@@ -8,7 +8,6 @@ class nsextras:
         self.bot = bot
 
     @commands.command()
-    @checks.admin_or_permissions(manage_server=True)
     async def say(self, *, text):
         """Displays what is said after say"""
 
