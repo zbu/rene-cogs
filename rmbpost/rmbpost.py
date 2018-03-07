ca67976019ca707842f035e8d7a286510e8aa804
zbu <zbu@users.noreply.github.com>
2018-03-06 23:38:05 -0500

    Add files via upload

--- rmbpost/rmbpost.py
+++ rmbpost/rmbpost.py
@@ -20,8 +20,8 @@ class nsextras:
         #Your code will go here
         url = "https://www.nationstates.net/page=rmb/postid=" + id #build the web adress
         async with aiohttp.get(url) as response:
-        try:
             soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
             online = soupObject.find(class_='rmbmsg2-container').get_text()
             nation = soupObject.find(class_='nname').get_text()
             datetime = soupObject.find(class_='rmbdate').find('a').find('time').get_text()
