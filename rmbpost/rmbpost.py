5e16df9792b0f9faa8a31c700203cf8ba442cb6a
zbu <zbu@users.noreply.github.com>
2018-03-06 23:29:39 -0500

    Add files via upload

--- rmbpost/rmbpost.py
+++ rmbpost/rmbpost.py
@@ -24,15 +24,13 @@ class nsextras:
         try:
             online = soupObject.find(class_='rmbmsg2-container').get_text()
             nation = soupObject.find(class_='nname').get_text()
-            datetime = soupObject.find('time').get_text()
+            datetime = soupObject.find(class_='rmbdate').find('a').find('time').get_text()
             flagpic = soupObject.find(class_='rmbdate').find('img').attrs['src']
-            description = online
             footer_text = "Last active " + datetime
-            embed.title = "RMB Post"
             embed.set_author(name=nation, url="https://nationstates.net/" + nation)
             embed.set_thumbnail(url="https://nationstates.net" + flagpic)
             embed.set_footer(text=footer_text)
-            embed = discord.Embed(colour=0xCEFF00, description=description)
+            embed = discord.Embed(title="RMB Post", colour=0xCEFF00, description=online)
             await self.bot.say(embed=embed)
         except:
             await self.bot.say("This RMB Post does not exist.")
