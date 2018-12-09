65d6dc58b81a03d7342ff86f2082368555d3b84f
zbu <zbu@users.noreply.github.com>
2018-12-08 22:18:37 -0500

    Add files via upload

---
+++ rpnation/rpnation.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+try: # check if BeautifulSoup4 is installed
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except:
+    soupAvailable = False
+import aiohttp
+
+class nsextras:
+    """NS Extras"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rpnation(self, nation):
+        """Force RP Nation Data"""
+
+        #Your code will go here
+        url = "https://slink.be/rp/index.php?nation=" + nation + "&submit=null" #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            online = soupObject.get_text()
+            await self.bot.say(online)
+        except:
+            await self.bot.say("ERROR.")
+
+def setup(bot):
+    bot.add_cog(rpnation(bot))
