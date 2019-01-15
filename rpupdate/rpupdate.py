29e33f0a5b187f75375c717feb9ae6dc9abf27e5
zbu <zbu@users.noreply.github.com>
2019-01-14 21:10:13 -0500

    Add files via upload

--- rpupdate/rpupdate.py
+++ rpupdate/rpupdate.py
@@ -18,6 +18,7 @@ class nsextras:
         """Force RP Nation Info"""
 
         #Your code will go here
+		nation.replace(" ", "_")
         url = "https://slink.be/rp/startingdataupdate.php?nation=" + nation #build the web adress
         async with aiohttp.get(url) as response:
             soupObject = BeautifulSoup(await response.text(), "html.parser")
