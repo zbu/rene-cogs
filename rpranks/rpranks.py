8f064a0d0689659d970ddc87ce63b4d33922caa2
zbu <zbu@users.noreply.github.com>
2020-06-26 03:34:58 -0400

    Add files via upload

---
+++ rpranks/rpranks.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+try: # check if BeautifulSoup4 is installed
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except:
+    soupAvailable = False
+import aiohttp
+
+class nsextras:
+    """NS Extras"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rpnation(self, *, sort_type):
+        """Force RP Ranking Info"""
+
+        #Your code will go here
+        url = "https://iloo.cc/rp/discordranksapi.php?sort" + sort_type #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            online = soupObject.get_text()
+            await self.bot.say(online)
+        except:
+            await self.bot.say("ERROR.")
+
+def setup(bot):
+    bot.add_cog(nsextras(bot))
